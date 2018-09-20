4be3fab1756c6be51f7ed8677336bbe2c04eea72
Mirti <avalith@o2.pl>
2018-09-20 18:21:45 +0200

    Phpdocs

--- src/Repository/Exception/LoggerException.php
+++ src/Repository/Exception/LoggerException.php
@@ -8,6 +8,12 @@ use Throwable;
 
 class LoggerException extends \Exception
 {
+    /**
+     * LoggerException constructor.
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
     public function __construct(string $message = "Logger Error", int $code = 500, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
